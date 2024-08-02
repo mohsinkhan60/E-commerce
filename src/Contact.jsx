a0edc806292fe2907095d25c8234d91fa3a7a295
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-02 15:08:09 +0500

    add contect section

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,97 @@
-export const Contact = () => {
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .input {
+    font-size: 1.6rem;
+    font-weight: 400;
+    }
+
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
   return (
-    <div>Contact</div>
-  )
-}
-export default Contact
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d212779.51526422135!2d72.8533369655697!3d33.56169151859928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df948974419acb%3A0x984357e1632d30f!2sRawalpindi%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1722547257491!5m2!1sen!2s"
+        width="100%"
+        height="350"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="fast"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xgvwkloy"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              className="input"
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              className="input"
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              className="input"
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message... "
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -140,7 +140,6 @@ input, textarea{
     color: ${({ theme }) => theme.colors.black};
     padding: 1.6rem 2.4rem;
     border: 1px solid ${({ theme }) => theme.colors.border};
-    text-transform: uppercase;
    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
 }
     input[type="submit"]{
