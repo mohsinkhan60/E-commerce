2e0909d6489ef1a8fb24f5d818d8843448ba5fbd
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-09 18:12:43 +0500

    Fix deploymentt error

--- src/Reducer/cartReducer.jsx
+++ src/Reducer/cartReducer.jsx
@@ -108,7 +108,7 @@ export const cartReducer = (state = initialState, action) => {
   }
 
   if (action.type === "CART_TOTAL_ITEM") {
-    let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+    let updatedItemVal = state?.cart?.reduce((initialVal, curElem) => {
       let { amount } = curElem;
       initialVal = initialVal + amount;
       return initialVal;
